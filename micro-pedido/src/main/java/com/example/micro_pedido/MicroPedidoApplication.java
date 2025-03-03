eacee9c6712463ed3dff0f7b24d52ec257b31ec7
sddrouet <sddrouet@espe.edu.ec>
2025-03-03 14:53:38 -0500

    initial microservices config

---
+++ micro-pedido/src/main/java/com/example/micro_pedido/MicroPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPedidoApplication.class, args);
+	}
+
+}
---
+++ micro-pedido/src/test/java/com/example/micro_pedido/MicroPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micro_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
