0ece2bccc301d45fdbd903e11d83326dff102772
sddrouet <sddrouet@espe.edu.ec>
2025-03-09 01:04:50 -0500

    git add micro auth server

--- micro-auth-server/src/main/java/com/example/micro_auth_server/client/UserClient.java
+++ micro-auth-server/src/main/java/com/example/micro_auth_server/client/UserClient.java
@@ -2,10 +2,8 @@ package com.example.micro_auth_server.client;
 
 import com.example.micro_auth_server.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "micro-usuarios", url = "${USER_SERVICE_URL:http://localhost:8083}/users")
 public interface UserClient {
